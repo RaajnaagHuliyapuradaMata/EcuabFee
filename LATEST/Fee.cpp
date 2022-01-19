b2d94fbb65b19abb2b0b7452b12f625d25c15642
root <nagaraja.hm.0001@gmail.com>
2022-01-19 09:44:42 +0100

    Update: submodule Fee

--- LATEST/Fee.cpp
+++ LATEST/Fee.cpp
@@ -5,3 +5,39 @@ class_Fee Fee;
 FUNC(void, FEE_CODE) class_Fee::InitFunction(void){
 }
 
+FUNC(void, FEE_CODE) class_Fee::SetMode(void){
+}
+
+FUNC(void, FEE_CODE) class_Fee::Read(void){
+}
+
+FUNC(void, FEE_CODE) class_Fee::Write(void){
+}
+
+FUNC(void, FEE_CODE) class_Fee::Cancel(void){
+}
+
+FUNC(void, FEE_CODE) class_Fee::GetStatus(void){
+}
+
+FUNC(void, FEE_CODE) class_Fee::GetJobResult(void){
+}
+
+FUNC(void, FEE_CODE) class_Fee::InvalidateBlock(void){
+}
+
+FUNC(void, FEE_CODE) class_Fee::GetVersionInfo(void){
+}
+
+FUNC(void, FEE_CODE) class_Fee::EraseImmediateBlock(void){
+}
+
+FUNC(void, FEE_CODE) class_Fee::CbJobEndNotification(void){
+}
+
+FUNC(void, FEE_CODE) class_Fee::CbJobErrorNotification(void){
+}
+
+FUNC(void, FEE_CODE) class_Fee::MainFunction(void){
+}
+
--- LATEST/Fee.h
+++ LATEST/Fee.h
@@ -5,7 +5,19 @@
 
 class class_Fee{
    public:
-      FUNC(void, FEE_CODE) InitFunction(void);
+      FUNC(void, FEE_CODE) InitFunction           (void);
+      FUNC(void, FEE_CODE) SetMode                (void);
+      FUNC(void, FEE_CODE) Read                   (void);
+      FUNC(void, FEE_CODE) Write                  (void);
+      FUNC(void, FEE_CODE) Cancel                 (void);
+      FUNC(void, FEE_CODE) GetStatus              (void);
+      FUNC(void, FEE_CODE) GetJobResult           (void);
+      FUNC(void, FEE_CODE) InvalidateBlock        (void);
+      FUNC(void, FEE_CODE) GetVersionInfo         (void);
+      FUNC(void, FEE_CODE) EraseImmediateBlock    (void);
+      FUNC(void, FEE_CODE) CbJobEndNotification   (void);
+      FUNC(void, FEE_CODE) CbJobErrorNotification (void);
+      FUNC(void, FEE_CODE) MainFunction           (void);
 };
 
 extern class_Fee Fee;
