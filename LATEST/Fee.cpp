0e1fe3518e1ee1b72430734116e7e678eac77590
root <nagaraja.hm.0001@gmail.com>
2022-03-07 12:09:59 +0100

    Update: submodule Fee

--- LATEST/Fee.cpp
+++ LATEST/Fee.cpp
@@ -24,8 +24,6 @@
 /*****************************************************/
 class module_Fee:
       public abstract_module
-   ,  public interface_Fee_EcuM
-   ,  public interface_Fee_SchM
 {
    public:
       FUNC(void, FEE_CODE) InitFunction   (void);
@@ -44,10 +42,9 @@ class module_Fee:
 /*****************************************************/
 /* OBJECTS                                           */
 /*****************************************************/
-module_Fee Fee;
-
-interface_Fee_EcuM *EcuM_Client_ptr_Fee = &Fee;
-interface_Fee_SchM *SchM_Client_ptr_Fee = &Fee;
+module_Fee     Fee;
+infEcuMClient* gptrinfEcuMClient_Fee = &Fee;
+infSchMClient* gptrinfSchMClient_Fee = &Fee;
 
 /*****************************************************/
 /* FUNCTIONS                                         */
