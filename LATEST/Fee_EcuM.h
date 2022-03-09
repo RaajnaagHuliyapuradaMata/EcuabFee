f7649b4ffe7c5d628631f1c3c832daa3f4fee8b1
root <nagaraja.hm.0001@gmail.com>
2022-03-09 12:53:32 +0100

    Update: submodule Fee

--- LATEST/Fee_EcuM.h
+++ LATEST/Fee_EcuM.h
@@ -32,7 +32,7 @@
 /*****************************************************/
 /* OBJECTS                                           */
 /*****************************************************/
-extern infEcuMClient *gptrinfEcuMClient_Fee;
+extern infEcuMClient* gptrinfEcuMClient_Fee;
 
 /*****************************************************/
 /* FUNCTIONS                                         */
--- LATEST/Fee_SchM.h
+++ LATEST/Fee_SchM.h
@@ -32,7 +32,7 @@
 /*****************************************************/
 /* OBJECTS                                           */
 /*****************************************************/
-extern infSchMClient *gptrinfSchMClient_Fee;
+extern infSchMClient* gptrinfSchMClient_Fee;
 
 /*****************************************************/
 /* FUNCTIONS                                         */
