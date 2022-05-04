cf2d130286df2e4ed91f4c58484ccaa56e982d05
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-04 09:01:04 +0000

    Update: AUTOSAR basic software ECU abstraction module Fee

--- LATEST/Fee.cpp
+++ LATEST/Fee.cpp
@@ -113,7 +113,7 @@ FUNC(void, FEE_CODE) module_Fee::InitFunction(
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  FEE_E_UNINIT
       );
 #endif
    }
@@ -133,7 +133,7 @@ FUNC(void, FEE_CODE) module_Fee::DeInitFunction(void){
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  FEE_E_UNINIT
       );
 #endif
    }
@@ -152,7 +152,7 @@ FUNC(void, FEE_CODE) module_Fee::MainFunction(void){
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  FEE_E_UNINIT
       );
 #endif
    }
