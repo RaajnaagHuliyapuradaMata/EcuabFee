c2be827ef8b89dddca52cf26c61be6c06c4a13c6
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-31 12:04:54 +0200

    Update: AUTOSAR ECU abstraction submodule - Fee

--- LATEST/Fee.cpp
+++ LATEST/Fee.cpp
@@ -32,7 +32,7 @@
 /* TYPEDEFS                                                                   */
 /******************************************************************************/
 class module_Fee:
-   INTERFACES_EXPORTED_FEE
+      INTERFACES_EXPORTED_FEE
       public abstract_module
    ,  public class_Fee_Functionality
 {
