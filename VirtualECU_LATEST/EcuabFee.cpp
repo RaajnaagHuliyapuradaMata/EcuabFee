dc3e7b25527195c10b2495c929c1d807cd2a87b5
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-10-24 22:04:50 +0200

    Update: AUTOSAR basic software ECU abstraction module for flash EEPROM emulation

--- VirtualECU_LATEST/EcuabFee.cpp
+++ VirtualECU_LATEST/EcuabFee.cpp
@@ -1,5 +1,5 @@
 /******************************************************************************/
-/* File   : EcuabFee.cpp                                                           */
+/* File   : EcuabFee.cpp                                                      */
 /* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
 /******************************************************************************/
 
@@ -13,8 +13,8 @@
 /******************************************************************************/
 /* #DEFINES                                                                   */
 /******************************************************************************/
-#define ECUABFEE_AR_RELEASE_VERSION_MAJOR                                           4
-#define ECUABFEE_AR_RELEASE_VERSION_MINOR                                           3
+#define ECUABFEE_AR_RELEASE_VERSION_MAJOR                                      4
+#define ECUABFEE_AR_RELEASE_VERSION_MINOR                                      3
 
 /******************************************************************************/
 /* MACROS                                                                     */
