81978920ec26aedf8717e5596ab5a729ab0faec5
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-01-24 21:51:51 +0100

    Update: AUTOSAR basic software ECU abstraction module for flash EEPROM emulation

---
+++ InfineonTLE9879EvalKit_R2023/infEcuabFee_ServiceNvM.hpp
@@ -0,0 +1,19 @@
+#pragma once
+/******************************************************************************/
+/* File   : infEcuabFee_ServiceNvM.hpp                                        */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define CONSTECUABFEE_AR_RELEASE_VERSION_MAJOR                                 4
+#define CONSTECUABFEE_AR_RELEASE_VERSION_MINOR                                 3
+
+#define Const_DefaultInit_ConstEcuabFee                                                                            \
+   ,  {CONSTECUABFEE_AR_RELEASE_VERSION_MAJOR, CONSTECUABFEE_AR_RELEASE_VERSION_MINOR, 0x00,0xFF,0x01,'0','1','0'} \
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
