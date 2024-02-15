dc8bcbe6648fe57b7d84d7e105a2d6f1ba612d5e
NagarajaHuliyapuradaMata <nagaraja.hm.0001@gmail.com>
2024-02-15 11:42:32 +0100

    Daily back-up

--- rECUTpmsHW6.2_TARGET/b_git_push.sh
+++ rECUTpmsHW6.2_TARGET/b_git_push.sh
@@ -1,4 +1,4 @@
 git add .
 git status
-git commit -m "AUTOSAR Migration Activities - EcuReal_vBcRsBpTpms_TARGET - EcuabFee"
+git commit -m "AUTOSAR Migration Activities - rEcu_vBcRsBpTpms_TARGET - EcuabFee"
 git push
